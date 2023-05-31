241f153ed6373a47f9a38a3f68e52c2c756799ee
josephchinedu <josephchinedu34@gmail.com>
2023-05-31 23:08:40 +0100

    update

---
+++ python/SORT_CHARACTERS_BY_FREQUENCY/README.md
@@ -0,0 +1,2 @@
+# SORT CHARACTERS BY FREQUENCY
+Explanation of the problem: https://gist.github.com/adewolejosh/074ab7a211ffabfe9f73e437c1e832ca
---
+++ python/SORT_CHARACTERS_BY_FREQUENCY/index.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        s = sorted(dict(Counter(s)).items(), key=lambda x: (x[1], x[0]), reverse=True)
+        return "".join(x[0]*x[1] for x in s)
